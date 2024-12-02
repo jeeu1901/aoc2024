89c921611d75bf2fcae4570de9401dba0055c027
jeeu1901 <jessie930818@gmail.com>
2024-12-03 00:56:38 +0100

    m

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Result day1.1 : " + Day1.partOne());
-//        System.out.println("Result day1.2 : " + Day1.partTwo());
+        System.out.println("Result day1.1 : " + Day1.partOne());
+        System.out.println("Result day1.2 : " + Day1.partTwo());
 
         System.out.println("Result day2.1 : " + Day2.partOne());
         System.out.println("Result day2.2 : " + Day2.partTwo());
